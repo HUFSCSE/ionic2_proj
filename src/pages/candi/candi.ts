d8e73b97eb25782de808e5401ca6f35ca9375f18
csbok <cesilphp@hotmail.com>
2017-03-14 22:06:13 +0900

    데이터를 angularfire2 대신 http를 사용하여 .json url로 갖고오도록 수정

--- src/pages/candi/candi.ts
+++ src/pages/candi/candi.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {DetailPage} from "../detail/detail";
-import {AngularFire, FirebaseListObservable} from 'angularfire2';
+import { Http } from '@angular/http';
 
 /*
   Generated class for the Candi page.
@@ -15,10 +15,19 @@ import {AngularFire, FirebaseListObservable} from 'angularfire2';
 })
 export class CandiPage {
   selected: any;
-  candis:FirebaseListObservable<any>;
+  candis = [];
 
-  constructor(af:AngularFire, public navCtrl: NavController, public navParams: NavParams) {
-    this.candis = af.database.list('/candidate');
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
+    this.http.get('https://ionic2study-a2337.firebaseio.com/candidate.json').map(res => res.json()).subscribe(data => {
+      // firebase에서 object 타입으로 받아온것을 array로 변환한다. (뷰에서 ngFor로 돌리기 위해)
+      let arr = [];
+
+      for(let x in data){
+        data[x].$key = x;
+        arr.push(data[x]);
+      }
+      this.candis = arr;
+    });
   }
 
   candiChoice(){

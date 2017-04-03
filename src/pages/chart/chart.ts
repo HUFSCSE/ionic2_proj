8ea845b64ac9650094fce7f9ddbcc82b51ac5ac7
csbok <cesilphp@hotmail.com>
2017-04-03 19:12:19 +0900

    firebase에서 차트 데이터 갖고와서 바그래프 그리도록 수정 이전 주, 다음 주 그래프 그려줄 수 있도록 수정

--- src/pages/chart/chart.ts
+++ src/pages/chart/chart.ts
@@ -1,6 +1,7 @@
 import {Component, ViewChild} from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {NavController, NavParams, ToastController} from 'ionic-angular';
 import { Chart } from 'chart.js';
+import {Http} from "@angular/http";
 
 /*
   Generated class for the Chart page.
@@ -15,20 +16,59 @@ import { Chart } from 'chart.js';
 export class ChartPage {
   @ViewChild('barCanvas') barCanvas;
   barChart: any;
+  chartData: Array<any>;
+  weekIndex: number;
+  weekIndexMax: number;
 
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http, public toastCtrl: ToastController) {
+  }
 
   ionViewDidLoad() {
+    this.http.get('https://ionic2study-a2337.firebaseio.com/chart/weekly.json').map(res => res.json()).subscribe(serverData => {
+      this.weekIndexMax = serverData.length-1;
+      this.weekIndex = serverData.length-1;
+      this.chartData = serverData;
+      this.chartGenerate();
+    }); // http
+  }
 
+  nextWeek() {
+    if (this.weekIndex < this.weekIndexMax) {
+      this.weekIndex++;
+      this.chartGenerate();
+    } else {
+      this.toastCtrl.create({
+        message: '더이상 다음 데이터가 없습니다.',
+        duration: 3000,
+        position: 'top'
+      }).present();
+    }
+
+  }
+
+  prevWeek() {
+    if (this.weekIndex > 0) {
+      this.weekIndex--;
+      this.chartGenerate();
+    } else {
+      this.toastCtrl.create({
+        message: '더이상 이전 데이터가 없습니다.',
+        duration: 3000,
+        position: 'top'
+      }).present();
+    }
+
+  }
+
+  chartGenerate() {
     this.barChart = new Chart(this.barCanvas.nativeElement, {
 
       type: 'bar',
       data: {
         labels: ["문재인", "안희정", "안철수", "이재명", "홍준표", "심상정", "유승민", "손학규", "황교안", "반기문", "박원순"],
         datasets: [{
-          label: '여론조사 지지도',
-          data: [34.9, 12.1, 18.7, 10.0, 7.5, 2.9, 2.9, 1.7, 0, 0, 0],
+          label: '여론조사 지지도(%)',
+          data: this.chartData[this.weekIndex],
           backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
             'rgba(54, 162, 235, 0.2)',
@@ -58,6 +98,6 @@ export class ChartPage {
         }
       }
 
-    });
+    }); // chart
   }
 }

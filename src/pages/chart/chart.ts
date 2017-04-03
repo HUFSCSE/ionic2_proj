87bfa6a1292ba91bde622ce705c062f6235c8288
csbok <cesilphp@hotmail.com>
2017-04-03 20:02:34 +0900

    후보자 목록, 주간 지지율 기간 목록을 firebase에서 갖고오도록 수정 각 후보자의 사진을 등록 (출처 : 나무위키) 지지율 그래프 밑에 각 후보의 지지율을 리스트형태로 표현

--- src/pages/chart/chart.ts
+++ src/pages/chart/chart.ts
@@ -19,22 +19,35 @@ export class ChartPage {
   chartData: Array<any>;
   weekIndex: number;
   weekIndexMax: number;
+  labelList: Array<string>;
+  label: string;
+  candidateList: Array<string>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http, public toastCtrl: ToastController) {
   }
 
   ionViewDidLoad() {
+    this.http.get('https://ionic2study-a2337.firebaseio.com/chart/weeklyLabel.json').map(res => res.json()).subscribe(labelData => {
+      this.labelList = labelData;
+      this.label = labelData[labelData.length-1];
+    });
+
     this.http.get('https://ionic2study-a2337.firebaseio.com/chart/weekly.json').map(res => res.json()).subscribe(serverData => {
       this.weekIndexMax = serverData.length-1;
       this.weekIndex = serverData.length-1;
       this.chartData = serverData;
-      this.chartGenerate();
+
+      this.http.get('https://ionic2study-a2337.firebaseio.com/chart/candidate.json').map(res => res.json()).subscribe(candidateList => {
+        this.candidateList = candidateList;
+        this.chartGenerate();
+      });
     }); // http
   }
 
   nextWeek() {
     if (this.weekIndex < this.weekIndexMax) {
       this.weekIndex++;
+      this.label = this.labelList[this.weekIndex];
       this.chartGenerate();
     } else {
       this.toastCtrl.create({
@@ -49,6 +62,7 @@ export class ChartPage {
   prevWeek() {
     if (this.weekIndex > 0) {
       this.weekIndex--;
+      this.label = this.labelList[this.weekIndex];
       this.chartGenerate();
     } else {
       this.toastCtrl.create({
@@ -61,11 +75,14 @@ export class ChartPage {
   }
 
   chartGenerate() {
+    if (this.barChart) {
+      this.barChart.destroy();
+    }
     this.barChart = new Chart(this.barCanvas.nativeElement, {
 
       type: 'bar',
       data: {
-        labels: ["문재인", "안희정", "안철수", "이재명", "홍준표", "심상정", "유승민", "손학규", "황교안", "반기문", "박원순"],
+        labels: this.candidateList,
         datasets: [{
           label: '여론조사 지지도(%)',
           data: this.chartData[this.weekIndex],
